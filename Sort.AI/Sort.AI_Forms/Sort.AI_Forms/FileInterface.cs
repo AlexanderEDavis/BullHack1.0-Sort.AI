0facfc7f15ed6c9b94b355a6fdcb99c1b2aa818a
jEdwardz <jamie.edwards5454@gmail.com>
2017-03-11 19:03:04 +0000

    Refined Settings Form
    Settings form layout and properties refined

---
+++ Sort.AI/Sort.AI_Forms/Sort.AI_Forms/FileInterface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+public class FileInterface
+{
+    private void PopulateFromFolder(DirectoryInfo di, String basePath)
+    {
+        //Iterate through files in directory 'di'
+        foreach (FileInfo file in di.GetFiles())
+        {
+
+        }
+        //Recursively call PopulateFromFolder on each directory in root directory
+        foreach (DirectoryInfo dir in di.GetDirectories())
+        {
+            PopulateFromFolder(dir, basePath);
+        }
+    }
+
+    private void ProgramFolder(string path)
+    {
+        try
+        {
+            //Check if directory already exists
+            if (Directory.Exists(path))
+            {
+                return;
+            }
+            //Create directory at path
+            DirectoryInfo di = Directory.CreateDirectory(path);
+        }
+        //Catch errors and display message box with error message
+        catch (Exception e)
+        {
+            MessageBox.Show(String.Format("The process failed: {0}", e.ToString()), "Folder creation failed");
+        }
+        finally { }
+    }
+
+    private void FileReader(FileInfo file)
+    {
+
+    }
+}
+
--- Sort.AI/Sort.AI_Forms/Sort.AI_Forms/Form1.Designer.cs
+++ Sort.AI/Sort.AI_Forms/Sort.AI_Forms/Form1.Designer.cs
@@ -31,104 +31,140 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtSourceLocation = new System.Windows.Forms.TextBox();
             this.txtDestLocation = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblDestination = new System.Windows.Forms.Label();
+            this.btnSetSource = new System.Windows.Forms.Button();
+            this.btnSetDestination = new System.Windows.Forms.Button();
+            this.btnForceSort = new System.Windows.Forms.Button();
             this.sourceDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.destinationDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.grpInfo = new System.Windows.Forms.GroupBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtLogFiles = new System.Windows.Forms.TextBox();
+            this.lblLogFiles = new System.Windows.Forms.Label();
+            this.grpInfo.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtSourceLocation
             // 
-            this.txtSourceLocation.Location = new System.Drawing.Point(81, 94);
+            this.txtSourceLocation.Location = new System.Drawing.Point(83, 12);
             this.txtSourceLocation.Name = "txtSourceLocation";
             this.txtSourceLocation.Size = new System.Drawing.Size(210, 20);
-            this.txtSourceLocation.TabIndex = 0;
+            this.txtSourceLocation.TabIndex = 1;
             // 
             // txtDestLocation
             // 
-            this.txtDestLocation.Location = new System.Drawing.Point(81, 157);
+            this.txtDestLocation.Location = new System.Drawing.Point(83, 52);
             this.txtDestLocation.Name = "txtDestLocation";
             this.txtDestLocation.Size = new System.Drawing.Size(210, 20);
-            this.txtDestLocation.TabIndex = 1;
+            this.txtDestLocation.TabIndex = 3;
             // 
-            // label1
+            // lblSource
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 97);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Source:";
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(14, 15);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(41, 13);
+            this.lblSource.TabIndex = 2;
+            this.lblSource.Text = "Source";
             // 
-            // label2
+            // lblDestination
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 160);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Destination:";
+            this.lblDestination.AutoSize = true;
+            this.lblDestination.Location = new System.Drawing.Point(14, 55);
+            this.lblDestination.Name = "lblDestination";
+            this.lblDestination.Size = new System.Drawing.Size(60, 13);
+            this.lblDestination.TabIndex = 3;
+            this.lblDestination.Text = "Destination";
             // 
-            // button1
+            // btnSetSource
             // 
-            this.button1.Location = new System.Drawing.Point(297, 92);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Browse";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnSetSource.Location = new System.Drawing.Point(299, 10);
+            this.btnSetSource.Name = "btnSetSource";
+            this.btnSetSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSetSource.TabIndex = 2;
+            this.btnSetSource.Text = "Browse";
+            this.btnSetSource.UseVisualStyleBackColor = true;
+            this.btnSetSource.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // btnSetDestination
             // 
-            this.button2.Location = new System.Drawing.Point(297, 155);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "Browse";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnSetDestination.Location = new System.Drawing.Point(299, 50);
+            this.btnSetDestination.Name = "btnSetDestination";
+            this.btnSetDestination.Size = new System.Drawing.Size(75, 23);
+            this.btnSetDestination.TabIndex = 4;
+            this.btnSetDestination.Text = "Browse";
+            this.btnSetDestination.UseVisualStyleBackColor = true;
+            this.btnSetDestination.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // btnForceSort
             // 
-            this.button3.Location = new System.Drawing.Point(155, 213);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "Sort";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnForceSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForceSort.Location = new System.Drawing.Point(299, 92);
+            this.btnForceSort.Name = "btnForceSort";
+            this.btnForceSort.Size = new System.Drawing.Size(75, 43);
+            this.btnForceSort.TabIndex = 5;
+            this.btnForceSort.Text = "Force Sort";
+            this.btnForceSort.UseVisualStyleBackColor = true;
             // 
-            // label3
+            // grpInfo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(103, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(179, 56);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Sort.AI";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.grpInfo.Controls.Add(this.lblInfo);
+            this.grpInfo.Location = new System.Drawing.Point(17, 87);
+            this.grpInfo.Name = "grpInfo";
+            this.grpInfo.Size = new System.Drawing.Size(276, 100);
+            this.grpInfo.TabIndex = 7;
+            this.grpInfo.TabStop = false;
+            this.grpInfo.Text = "Information";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Location = new System.Drawing.Point(7, 20);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(263, 77);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "Set the Source folder as your folder that needs to be sorted by Sort.AI\r\n\r\nSet th" +
+    "e Destination folder to the folder you would like the sorted files to be placed " +
+    "once organised by Sort.AI";
+            // 
+            // txtLogFiles
+            // 
+            this.txtLogFiles.Location = new System.Drawing.Point(400, 31);
+            this.txtLogFiles.Multiline = true;
+            this.txtLogFiles.Name = "txtLogFiles";
+            this.txtLogFiles.ReadOnly = true;
+            this.txtLogFiles.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLogFiles.Size = new System.Drawing.Size(370, 218);
+            this.txtLogFiles.TabIndex = 6;
+            // 
+            // lblLogFiles
+            // 
+            this.lblLogFiles.AutoSize = true;
+            this.lblLogFiles.Location = new System.Drawing.Point(397, 15);
+            this.lblLogFiles.Name = "lblLogFiles";
+            this.lblLogFiles.Size = new System.Drawing.Size(49, 13);
+            this.lblLogFiles.TabIndex = 2;
+            this.lblLogFiles.Text = "Log Files";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 261);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(782, 261);
+            this.Controls.Add(this.txtLogFiles);
+            this.Controls.Add(this.grpInfo);
+            this.Controls.Add(this.btnForceSort);
+            this.Controls.Add(this.btnSetDestination);
+            this.Controls.Add(this.btnSetSource);
+            this.Controls.Add(this.lblDestination);
+            this.Controls.Add(this.lblLogFiles);
+            this.Controls.Add(this.lblSource);
             this.Controls.Add(this.txtDestLocation);
             this.Controls.Add(this.txtSourceLocation);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Sort.AI";
+            this.Text = "Sort.AI Settings";
+            this.grpInfo.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,14 +174,17 @@
 
         private System.Windows.Forms.TextBox txtSourceLocation;
         private System.Windows.Forms.TextBox txtDestLocation;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblDestination;
+        private System.Windows.Forms.Button btnSetSource;
+        private System.Windows.Forms.Button btnSetDestination;
+        private System.Windows.Forms.Button btnForceSort;
         private System.Windows.Forms.FolderBrowserDialog sourceDialog;
         private System.Windows.Forms.FolderBrowserDialog destinationDialog;
+        private System.Windows.Forms.GroupBox grpInfo;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtLogFiles;
+        private System.Windows.Forms.Label lblLogFiles;
     }
 }
 
