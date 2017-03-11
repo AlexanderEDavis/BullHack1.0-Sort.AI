a6f60796aa9cc5377e1a34daf9a16db2a6f24f44
Alexander Davis <alexanderedavis@outlook.com>
2017-03-11 22:22:07 +0000

    Tidying of Project Files

---
+++ Everything Fucked/Sort.AI_Forms/Sort.AI_Forms/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sort.AI_Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = sourceDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                txtSourceLocation.Text = sourceDialog.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = destinationDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                txtDestLocation.Text = destinationDialog.SelectedPath;
+            }
+        }
+    }
+}
---
+++ Sort.AI/Sort.AI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Sort.AI
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new SortAiService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
